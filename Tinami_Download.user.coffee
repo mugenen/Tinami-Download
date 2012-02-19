0470c6612b743deb12db396f32abd0ffa84d3c2e
oga <8monkey.theorem@gmail.com>
2012-02-19 20:21:03 +0900

    Version 2.2, Switched to CoffeeScript.

---
+++ Tinami_Download.user.coffee
@@ -0,0 +1,99 @@
+if(!document.getElementById("view"))
+    return;
+
+dispatchMouseEvents = (opt) ->
+    ###
+    Alt + Click
+    See: http://d.hatena.ne.jp/Griever/20100904/1283603283
+    ###
+    evt = document.createEvent('MouseEvents');
+    evt.initMouseEvent(opt.type, opt.canBubble||true, opt.cancelable||true, opt.view||window, 
+                       opt.detail||0, opt.screenX||0, opt.screenY||0, opt.clientX||0, opt.clientY||0, 
+                       opt.ctrlKey||false, opt.altKey||false, opt.shiftKey||false, opt.metaKey||false, 
+                       opt.button||0, opt.relatedTarget||null);
+    opt.target.dispatchEvent(evt);
+
+getImageTitle = () ->
+    tag = document.querySelector('.viewdata span')
+    if tag?
+        tag.textContent;
+    else
+        null
+
+getImageCreator = () ->
+    tag = document.querySelector('.prof strong')
+    if tag?
+        tag.textContent;
+    else
+        null
+
+getImageID = () ->
+    document.URL.substring(document.URL.lastIndexOf('/') + 1)
+
+getFileName = () ->
+    creator = getImageCreator()
+    title = getImageTitle()
+    id = getImageID()
+    if creator? and title? and id?
+        creator + ' - ' + title + '(' + id + ')'
+    else
+        null
+
+download = (url, filename) ->
+    toClick = document.createElement('a');
+    toClick.setAttribute('href', url);
+    toClick.setAttribute('download', filename);
+    dispatchMouseEvents({type:'click', altKey:false, target:toClick, button:0});
+
+addLink = (main) ->
+    a = document.createElement('a');
+    a.addEventListener('click', main, false);
+
+    img = document.createElement('img')
+    img.setAttribute('src', chrome.extension.getURL('download.png'));
+    img.setAttribute('draggable', false)
+    a.appendChild(img);
+    a.setAttribute('href', 'javascript:void(0);');
+
+    parent = document.getElementById('mv');
+    parent.appendChild(a);
+
+
+type = document.querySelector('#view img').getAttribute("src")
+filename = getFileName()
+
+if not filename?
+    return
+
+if type == '/img/job/view/mo.gif' or type == '/img/job/view/il.gif'
+    if document.querySelector('.viewbody img').style.cursor == 'pointer'#拡大可能な一枚絵
+        ifr = document.createElement('iframe');
+        ifr.setAttribute('id', 'TD_temp');
+        document.body.appendChild(ifr);
+
+        ifr.onload = () ->
+            main = () ->
+                original = ifr.contentDocument.getElementsByTagName("img")[0].getAttribute("src");
+                download(original, filename)
+                @removeEventListener('click', main);
+            addLink(main)
+        
+        form = document.getElementById('open_original_content')
+        form.setAttribute('target', 'TD_temp');
+        form.submit();
+        form.removeAttribute("target");
+    else#拡大できない一枚絵
+        main = () ->
+            images = document.querySelector('.viewbody').querySelectorAll('img')
+            download(images[0].getAttribute("src"), filename)
+            this.removeEventListener('click', main);
+        addLink(main)
+else if type == '/img/job/view/ma.gif'#漫画の場合
+    main = () ->
+        images = document.querySelector('.viewbody').querySelectorAll('img')
+        for image, i in images
+            download(image.getAttribute('src'), filename + ' - ' + (i + 1))
+        this.removeEventListener('click', main);
+    addLink(main)
+
+
--- package/Tinami_Download.user.js
+++ package/Tinami_Download.user.js
@@ -1,90 +1,126 @@
-'use strict';
-(function(){
-  if(!document.getElementById("view")) {
-      return;
-  }
-  var type = document.getElementById("view").getElementsByTagName("img")[0].getAttribute("src");
-  //Alt + Click
-  //See: http://d.hatena.ne.jp/Griever/20100904/1283603283
-  function dispatchMouseEvents(opt) {
-    var evt = document.createEvent('MouseEvents');
-    evt.initMouseEvent(opt.type, opt.canBubble||true, opt.cancelable||true, opt.view||window, 
-                       opt.detail||0, opt.screenX||0, opt.screenY||0, opt.clientX||0, opt.clientY||0, 
-                       opt.ctrlKey||false, opt.altKey||false, opt.shiftKey||false, opt.metaKey||false, 
-                       opt.button||0, opt.relatedTarget||null);
-    opt.target.dispatchEvent(evt);
-    return evt;
-  }
-  var mv = document.getElementById("mv");
-  var a = document.createElement("a");
-  var img = document.createElement("img")
-  img.setAttribute("src", chrome.extension.getURL("download.png"));
-  img.setAttribute("draggable", false)
-  a.appendChild(img);
-  a.setAttribute("href", "javascript:void(0);");
+(function() {
+  var addLink, dispatchMouseEvents, download, filename, form, getFileName, getImageCreator, getImageID, getImageTitle, ifr, main, type;
 
-  var filename = function (e) {
-    var title = document.getElementsByClassName('viewdata')[0].getElementsByTagName('span')[0].textContent
-    var creator = document.getElementsByClassName('prof')[0].getElementsByTagName('strong')[0].textContent;
-    var id = document.URL.substring(document.URL.lastIndexOf("/") + 1);
-  
-    var filename = creator + " - " + title + "(" + id + ")";
-    return filename;
-  }
+  if (!document.getElementById("view")) return;
 
-  if((type == "/img/job/view/mo.gif") || (type == "/img/job/view/il.gif")) {
-    if(document.getElementsByClassName("viewbody")[0].getElementsByTagName("img")[0].style.cursor == "pointer") {
-      //Load image in iframe
-      var ifr = document.createElement("iframe");
-      ifr.setAttribute("id", "TD_temp");
-      document.body.appendChild(ifr);
-      document.getElementById("open_original_content").setAttribute("target", "TD_temp");
+  dispatchMouseEvents = function(opt) {
+    /*
+        Alt + Click
+        See: http://d.hatena.ne.jp/Griever/20100904/1283603283
+    */
+    var evt;
+    evt = document.createEvent('MouseEvents');
+    evt.initMouseEvent(opt.type, opt.canBubble || true, opt.cancelable || true, opt.view || window, opt.detail || 0, opt.screenX || 0, opt.screenY || 0, opt.clientX || 0, opt.clientY || 0, opt.ctrlKey || false, opt.altKey || false, opt.shiftKey || false, opt.metaKey || false, opt.button || 0, opt.relatedTarget || null);
+    return opt.target.dispatchEvent(evt);
+  };
 
-      ifr.onload = function(){
-        var main = function(){
-          var toClick = document.createElement("a");
-          var original = ifr.contentDocument.getElementsByTagName("img")[0].getAttribute("src");
-          toClick.setAttribute("href", original);
-          toClick.setAttribute('download', filename());
-          dispatchMouseEvents({ type:'click', altKey:false, target:toClick, button:0 });
-          this.removeEventListener('click', main);
-        };
-        if(main){
-          a.addEventListener("click", main, false);
-        }
-      };
-      
-      document.getElementById("open_original_content").submit();
-      document.getElementById("open_original_content").removeAttribute("target");
+  getImageTitle = function() {
+    var tag;
+    tag = document.querySelector('.viewdata span');
+    if (tag != null) {
+      return tag.textContent;
     } else {
-      var main = function(){
-        var images = document.getElementsByClassName("viewbody")[0].getElementsByTagName("img");
-        var toClick = document.createElement("a");
-        toClick.setAttribute("href", images[0].getAttribute("src"));
-        toClick.setAttribute('download', filename());
-        dispatchMouseEvents({ type:'click', altKey:false, target:toClick, button:0 });
-        this.removeEventListener('click', main);
-      };
-    }
-  } else if(type == "/img/job/view/ma.gif") {
-    var main = function(){
-      var images = document.getElementsByClassName("viewbody")[0].getElementsByTagName("img");
-      for(var i = 0; i < images.length; i++) {
-        var toClick = document.createElement("a");
-        toClick.setAttribute("href", images[i].getAttribute('src'));
-        toClick.setAttribute('download', filename() + ' - ' + (i + 1));
-        dispatchMouseEvents({ type:'click', altKey:false, target:toClick, button:0 });
-      }
-      this.removeEventListener('click', main);
-    };
-  }
-
-  var click = function(){
-    if(main){
-      a.addEventListener("click", main, false);
+      return null;
     }
   };
-  click();
-  
-  mv.appendChild(a);
-})();
+
+  getImageCreator = function() {
+    var tag;
+    tag = document.querySelector('.prof strong');
+    if (tag != null) {
+      return tag.textContent;
+    } else {
+      return null;
+    }
+  };
+
+  getImageID = function() {
+    return document.URL.substring(document.URL.lastIndexOf('/') + 1);
+  };
+
+  getFileName = function() {
+    var creator, id, title;
+    creator = getImageCreator();
+    title = getImageTitle();
+    id = getImageID();
+    if ((creator != null) && (title != null) && (id != null)) {
+      return creator + ' - ' + title + '(' + id + ')';
+    } else {
+      return null;
+    }
+  };
+
+  download = function(url, filename) {
+    var toClick;
+    toClick = document.createElement('a');
+    toClick.setAttribute('href', url);
+    toClick.setAttribute('download', filename);
+    return dispatchMouseEvents({
+      type: 'click',
+      altKey: false,
+      target: toClick,
+      button: 0
+    });
+  };
+
+  addLink = function(main) {
+    var a, img, parent;
+    a = document.createElement('a');
+    a.addEventListener('click', main, false);
+    img = document.createElement('img');
+    img.setAttribute('src', chrome.extension.getURL('download.png'));
+    img.setAttribute('draggable', false);
+    a.appendChild(img);
+    a.setAttribute('href', 'javascript:void(0);');
+    parent = document.getElementById('mv');
+    return parent.appendChild(a);
+  };
+
+  type = document.querySelector('#view img').getAttribute("src");
+
+  filename = getFileName();
+
+  if (!(filename != null)) return;
+
+  if (type === '/img/job/view/mo.gif' || type === '/img/job/view/il.gif') {
+    if (document.querySelector('.viewbody img').style.cursor === 'pointer') {
+      ifr = document.createElement('iframe');
+      ifr.setAttribute('id', 'TD_temp');
+      document.body.appendChild(ifr);
+      ifr.onload = function() {
+        var main;
+        main = function() {
+          var original;
+          original = ifr.contentDocument.getElementsByTagName("img")[0].getAttribute("src");
+          download(original, filename);
+          return this.removeEventListener('click', main);
+        };
+        return addLink(main);
+      };
+      form = document.getElementById('open_original_content');
+      form.setAttribute('target', 'TD_temp');
+      form.submit();
+      form.removeAttribute("target");
+    } else {
+      main = function() {
+        var images;
+        images = document.querySelector('.viewbody').querySelectorAll('img');
+        download(images[0].getAttribute("src"), filename);
+        return this.removeEventListener('click', main);
+      };
+      addLink(main);
+    }
+  } else if (type === '/img/job/view/ma.gif') {
+    main = function() {
+      var i, image, images, _len;
+      images = document.querySelector('.viewbody').querySelectorAll('img');
+      for (i = 0, _len = images.length; i < _len; i++) {
+        image = images[i];
+        download(image.getAttribute('src'), filename + ' - ' + (i + 1));
+      }
+      return this.removeEventListener('click', main);
+    };
+    addLink(main);
+  }
+
+}).call(this);
